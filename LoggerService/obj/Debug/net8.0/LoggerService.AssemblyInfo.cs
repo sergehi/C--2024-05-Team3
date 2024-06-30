5cc3c25fd172ce99923a4e1efb84fb3a58fc3e9f
FURYER <furyer@inbox.ru>
2024-06-30 19:30:48 +0500

    1

--- LoggerService/obj/Debug/net8.0/LoggerService.AssemblyInfo.cs
+++ LoggerService/obj/Debug/net8.0/LoggerService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23d4005f32566a28a9247e33598883e4b44a0d2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20d86d0878e7b374e2d11c38d408985249d4c15b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
