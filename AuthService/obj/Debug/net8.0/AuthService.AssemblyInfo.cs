83f1d24a07cef10220934098fdb3e44f153b5657
FURYER <furyer@inbox.ru>
2024-06-30 06:07:29 +0500

    Add Docker and Certificate

--- AuthService/obj/Debug/net8.0/AuthService.AssemblyInfo.cs
+++ AuthService/obj/Debug/net8.0/AuthService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23d4005f32566a28a9247e33598883e4b44a0d2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e80ab158f89e17b11e230c994e72fc5db65ccc30")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
