642b24f64c6ef82c3c4eb1846d6b4a25841881af
Shams Mehraliyeva <shams.mehralieva99@gmail.com>
2024-07-24 10:24:31 +0400

    feat: add common repositories

---
+++ Common/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Common.Repositories
+{
+    public interface IRepository<T>
+        where T : Entity
+    {
+        IQueryable<T> GetAll(bool noTracking = false);
+        Task<List<T>> GetAllAsync(CancellationToken cancellationToken, bool asNoTracking = false);
+        T Get(int id);
+        Task<T> GetAsync(int id, CancellationToken cancellationToken);
+        bool Delete(int id);
+        bool Delete(T entity);
+        bool DeleteRange(ICollection<T> entities);
+        void Update(T entity);
+        T Add(T entity);
+        Task<T> AddAsync(T entity);
+        Task AddRangeAsync(ICollection<T> entities);
+        void SaveChanges();
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
