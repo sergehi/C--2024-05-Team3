b260b7578cefccdd677c622480ce23e106d4b9b8
ShamsMehraliyeva <shams.mehralieva99@gmail.com>
2024-08-18 22:05:05 +0400

    feat: add repositories for main entites

---
+++ src/ChatService/Services/ChatService.Services.Repositories.Abstractions/IConversationRepository.cs
@@ -0,0 +1,9 @@
+﻿using ChatService.Entities;
+using Common.Repositories;
+
+namespace ChatService.Services.Repositories.Abstractions
+{
+    public interface IConversationRepository : IRepository<Conversation>
+    {
+    }
+}
