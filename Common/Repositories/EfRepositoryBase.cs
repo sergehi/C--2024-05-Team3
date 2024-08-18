154a22ccf32f5588e0f7a52fae916dafbc11ddeb
ShamsMehraliyeva <shams.mehralieva99@gmail.com>
2024-08-18 22:05:05 +0400

    feat: add repositories for main entites

--- Common/Repositories/EfRepositoryBase.cs
+++ Common/Repositories/EfRepositoryBase.cs
@@ -1,103 +1,146 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace Common.Repositories;
 
- public class EfRepositoryBase<T> : IRepository<T>
-         where T : BaseEntity
- {
-     public readonly DbContext Context;
-     private readonly DbSet<T> _entitySet;
+public class Repository<T> : IRepository<T>
+    where T : BaseEntity
+{
+    public readonly DbContext Context;
+    private readonly DbSet<T> _entitySet;
 
-     public EfRepositoryBase(DbContext context)
-     {
-         Context = context;
-         _entitySet = Context.Set<T>();
-     }
-     public virtual T Get(int id)
-     {
-         return _entitySet.Find(id);
-     }
-     public virtual async Task<T> GetAsync(int id, CancellationToken cancellationToken)
-     {
-         return await _entitySet.FindAsync((object)id);
-     }
+    public Repository(DbContext context)
+    {
+        Context = context;
+        _entitySet = Context.Set<T>();
+    }
 
-     public virtual IQueryable<T> GetAll(bool asNoTracking = false)
-     {
-         return asNoTracking ? _entitySet.AsNoTracking() : _entitySet;
-     }
+    public async Task<T?> GetAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await Context.Set<T>().FirstOrDefaultAsync(predicate);
+    }
 
-     public async Task<List<T>> GetAllAsync(CancellationToken cancellationToken, bool asNoTracking = false)
-     {
-         return await GetAll().ToListAsync(cancellationToken);
-     }
+    public IQueryable<T> GetWhere(Expression<Func<T, bool>> method,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? includes = null,
+        bool tracking = true)
+    {
+        var query = Context.Set<T>().Where(method);
+        if (includes != null)
+            query = includes(query);
+        if (!tracking)
+            query = query.AsNoTracking();
+        return query;
+    }
 
-     public virtual T Add(T entity)
-     {
-         var objToReturn = _entitySet.Add(entity);
-         return objToReturn.Entity;
-     }
-     public virtual async Task<T> AddAsync(T entity)
-     {
-         return (await _entitySet.AddAsync(entity)).Entity;
-     }
-     public virtual void AddRange(List<T> entities)
-     {
-         var enumerable = entities as IList<T> ?? entities.ToList();
-         _entitySet.AddRange(enumerable);
-     }
-     public virtual async Task AddRangeAsync(ICollection<T> entities)
-     {
-         if (entities == null || !entities.Any())
-         {
-             return;
-         }
-         await _entitySet.AddRangeAsync(entities);
-     }
+    public virtual T Get(int id)
+    {
+        return _entitySet.Find(id);
+    }
 
-     public virtual void Update(T entity)
-     {
-         Context.Entry(entity).State = EntityState.Modified;
-     }
+    public virtual async Task<T> GetAsync(int id)
+    {
+        return await _entitySet.FindAsync((object)id);
+    }
 
+    public virtual IQueryable<T> GetAll(bool asNoTracking = false)
+    {
+        return asNoTracking ? _entitySet.AsNoTracking() : _entitySet;
+    }
 
-     public virtual bool Delete(int id)
-     {
-         var obj = _entitySet.Find(id);
-         if (obj == null)
-         {
-             return false;
-         }
-         _entitySet.Remove(obj);
-         return true;
-     }
+    public async Task<List<T>> GetAllAsync(CancellationToken cancellationToken, bool asNoTracking = false)
+    {
+        return await GetAll().ToListAsync(cancellationToken);
+    }
 
-     public virtual bool Delete(T entity)
-     {
-         if (entity == null)
-         {
-             return false;
-         }
-         Context.Entry(entity).State = EntityState.Deleted;
-         return true;
-     }
+    public virtual T Add(T entity)
+    {
+        var objToReturn = _entitySet.Add(entity);
+        Context.SaveChanges();
+        return objToReturn.Entity;
+    }
 
-     public virtual bool DeleteRange(ICollection<T> entities)
-     {
-         if (entities == null || !entities.Any())
-         {
-             return false;
-         }
-         _entitySet.RemoveRange(entities);
-         return true;
-     }
-     public virtual void SaveChanges()
-     {
-         Context.SaveChanges();
-     }
+    public virtual async Task<T> AddAsync(T entity)
+    {
+        Context.Entry(entity).State = EntityState.Added;
+        await Context.SaveChangesAsync();
+        return entity;
+    }
 
-     public virtual async Task SaveChangesAsync(CancellationToken cancellationToken = default)
-     {
-         await Context.SaveChangesAsync(cancellationToken);
-     }
- }
+    public virtual void AddRange(List<T> entities)
+    {
+        var enumerable = entities as IList<T> ?? entities.ToList();
+        _entitySet.AddRange(enumerable);
+        Context.SaveChanges();
+    }
+
+    public virtual async Task AddRangeAsync(ICollection<T> entities)
+    {
+        if (entities == null || !entities.Any())
+        {
+            return;
+        }
+
+        _entitySet.AddRangeAsync(entities);
+        await Context.SaveChangesAsync();
+    }
+
+    public virtual void Update(T entity)
+    {
+        Context.Entry(entity).State = EntityState.Modified;
+        Context.SaveChanges();
+    }
+
+    public virtual async Task UpdateAsync(T entity)
+    {
+        Context.Entry(entity).State = EntityState.Modified;
+        await Context.SaveChangesAsync();
+    }
+
+    public virtual async Task UpdateRangeAsync(IEnumerable<T> entities)
+    {
+        foreach (var entity in entities)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+        }
+
+        await Context.SaveChangesAsync();
+    }
+
+    public virtual bool Delete(int id)
+    {
+        var obj = _entitySet.Find(id);
+        if (obj == null)
+        {
+            return false;
+        }
+
+        _entitySet.Remove(obj);
+        Context.SaveChanges();
+        return true;
+    }
+
+    public virtual bool Delete(T entity)
+    {
+        if (entity == null)
+        {
+            return false;
+        }
+
+        Context.Entry(entity).State = EntityState.Deleted;
+        Context.SaveChanges();
+        return true;
+    }
+
+    public virtual bool DeleteRange(ICollection<T> entities)
+    {
+        if (entities == null || !entities.Any())
+        {
+            return false;
+        }
+
+        _entitySet.RemoveRange(entities);
+        Context.SaveChanges();
+        return true;
+    }
+}
--- src/ChatService/ChatService.API/Registrator.cs
+++ src/ChatService/ChatService.API/Registrator.cs
@@ -1,5 +1,7 @@
 ﻿using ChatService.Infrastructure.EntityFramework;
 using ChatService.API.Settings;
+using ChatService.Infrastructure.Repositories.Implementations;
+using ChatService.Services.Repositories.Abstractions;
 
 namespace ChatService.API;
 
@@ -10,8 +12,19 @@ public static class Registrator
         var applicationSettings = configuration.Get<ApplicationSettings>();
         services.AddSingleton(applicationSettings)
                 .AddSingleton((IConfigurationRoot)configuration)
-                .ConfigureContext(applicationSettings.ConnectionString);
+                .ConfigureContext(applicationSettings.ConnectionString)
+                .InstallRepositories();
 
         return services;
+    }
+    
+    private static IServiceCollection InstallRepositories(this IServiceCollection serviceCollection)
+    {
+        serviceCollection
+            .AddTransient<IConversationRepository, ConversationRepository>()
+            .AddTransient<IMediaFileRepository, MediaFileRepository>()
+            .AddTransient<IMessageRepository, MessageRepository>()
+            .AddTransient<IReactionRepository, ReactionRepository>();
+        return serviceCollection;
     }
 }
