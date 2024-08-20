2d6f088c3432d22fb6d21bf6e44439c3076fc7cc
Токарев Виктор <tokarev@uralgeo.pem.ru>
2024-08-20 20:05:27 +0500

    1

---
+++ Logger/DAL/Logger.DataAccess.EntityFramework/LoggerContext.cs
@@ -0,0 +1,28 @@
+﻿using Logger.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Logger.DataAccess.EntityFramework
+{
+    public class LoggerContext : DbContext
+    {
+        public DbSet<Log> Logs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Log>()
+                .HasIndex(l => l.Time);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=31.128.43.53;Port=5432;Username=postgres;Database=LoggerService;Password=jomjcpFnXs");
+        }
+
+        public LoggerContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ Logger/DAL/Logger.DataAccess.EntityFramework/Migrations/20240820134723_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Logger.DataAccess.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Action = table.Column<int>(type: "integer", nullable: false),
+                    EntityId = table.Column<Guid>(type: "uuid", nullable: false),
+                    EntityPrimaryKey = table.Column<Guid>(type: "uuid", nullable: false),
+                    Entity = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Logs_Time",
+                table: "Logs",
+                column: "Time");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logs");
+        }
+    }
+}
