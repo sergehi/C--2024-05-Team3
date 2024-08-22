df9a09a4d0fde1af42df7366b2b80cbe1893f085
Токарев Виктор <tokarev@uralgeo.pem.ru>
2024-08-22 14:16:48 +0500

    1

--- Logger/DAL/Logger.DataAccess.Repositories.Implementations/LogRepository.cs
+++ Logger/DAL/Logger.DataAccess.Repositories.Implementations/LogRepository.cs
@@ -1,9 +1,8 @@
-﻿using Logger.DataAccess.Repositories.Abstractions;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Logger.DataAccess.Repositories.Implementations
 {
-    public class LogRepository : Common.Repositories.Repository<Entities.Log, long>, ILogRepository
+    public class LogRepository : Common.Repositories.Repository<Entities.Log, long>
     {
         public LogRepository(DbContext context) : base(context) { }
     }
