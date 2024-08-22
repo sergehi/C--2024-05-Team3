e7c962c99cad0101f8102d0adb45f408b136ca0c
Токарев Виктор <tokarev@uralgeo.pem.ru>
2024-08-22 16:06:21 +0500

    1

---
+++ Logger/BLL/Logger.BusinessLogic.Entities/Log/LogDTO.cs
@@ -0,0 +1,39 @@
+﻿using Logger.DataAccess.Entities;
+
+namespace Logger.BusinessLogic.Entities.Log
+{
+    public class LogDTO
+    {
+        public long Id { get; set; } = 0;
+
+        /// <summary>
+        /// Идентификатор пользователя.
+        /// </summary>
+        public Guid UserId { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Дата и время действия.
+        /// </summary>
+        public DateTime Time { get; set; } = DateTime.MinValue;
+
+        /// <summary>
+        /// Действие над объектом.
+        /// </summary>
+        public ELogAction Action { get; set; } = ELogAction.LA_None;
+
+        /// <summary>
+        /// Идентификатор сущности.
+        /// </summary>
+        public Guid EntityId { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Первичный ключ.
+        /// </summary>
+        public Guid EntityPrimaryKey { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Сущность.
+        /// </summary>
+        public string Entity { get; set; } = string.Empty;
+    }
+}
