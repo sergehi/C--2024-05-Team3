ddab5179213d655972b09e2deec250656d434e86
Токарев Виктор <tokarev@uralgeo.pem.ru>
2024-09-09 15:22:51 +0500

    123

--- Logger/LoggerService/Program.cs
+++ Logger/LoggerService/Program.cs
@@ -1,3 +1,4 @@
+using Logger.BusinessLogic.Services.Abstractions;
 using LoggerService;
 using LoggerService.Services;
 
@@ -6,10 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 builder.Services.AddServices(builder.Configuration);
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<LogService>();
+
+var rabbit = app.Services.GetService<RabbitService>();
+rabbit?.StartListening();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client.");
 app.Run();
