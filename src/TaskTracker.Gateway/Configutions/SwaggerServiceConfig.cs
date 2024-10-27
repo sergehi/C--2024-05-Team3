0956a8d735322129c27229c3af0b657ff086cdfe
ShamsMehraliyeva <shams.mehralieva99@gmail.com>
2024-10-27 20:32:24 +0400

    faat: add gateway project and chatservice grpc project

---
+++ src/TaskTracker.Gateway/Configutions/SwaggerServiceConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskTracker.Gateway.Configutions
+{
+    public class SwaggerServiceConfig
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Version { get; set; }
+        public string Endpoint { get; set; }
+    }
+}
---
+++ src/TaskTracker.Gateway/Controllers/Service1Controller.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestGrpcService1;
+
+namespace TaskTracker.Gateway.Controllers
+{
+    [ApiController]
+    [Route("/service1/Greeter")]
+    [ApiExplorerSettings(GroupName = "service1")]
+    public class Service1Controller : ControllerBase
+    {
+        private readonly Greeter.GreeterClient _client;
+
+        public Service1Controller(Greeter.GreeterClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet("sayhello1")]
+        public async Task<IActionResult> SayHello1(string name)
+        {
+
+            return Ok("1");
+        }
+
+        [HttpGet("sayhello")]
+        public async Task<IActionResult> SayHello(string name)
+        {
+            var response = await _client.SayHelloAsync(new HelloRequest { Name = name });
+            return Ok(response.Message);
+        }
+
+        [HttpGet("saygoodbye")]
+        public async Task<IActionResult> SayGoodbye(string name)
+        {
+            var response = await _client.SayGoodbyeAsync(new GoodbyeRequest { Name = name });
+            return Ok(response.Message);
+        }
+    }
+}
