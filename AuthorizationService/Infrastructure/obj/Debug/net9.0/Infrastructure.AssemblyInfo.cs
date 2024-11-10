4752ee1946e32c38620df78dab4e513a83d3d162
FURYER <furyer@inbox.ru>
2024-11-10 21:28:37 +0500

    Fixes

--- AuthorizationService/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ AuthorizationService/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecbf6a259b730eef74c8c81e0895163fabb1b96b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b1a583be63d753532dbef2da2dd92bc1b5056da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
