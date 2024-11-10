777057247ca1220480c9db59c99a5cbd0f1809a9
FURYER <furyer@inbox.ru>
2024-11-10 21:57:46 +0500

    Fix logs

---
+++ Logger/BLL/Logger.BusinessLogic.DTO/Log/FilterLogDTO.cs
@@ -0,0 +1,47 @@
+﻿using Logger.DataAccess.Entities;
+
+namespace Logger.BusinessLogic.DTO.Log
+{
+    public class FilterLogDTO
+    {
+        /// <summary>
+        /// Начало диапазона даты и времени действий.
+        /// </summary>
+        public DateTime BeginTime { get; set; } = DateTime.MinValue;
+
+        /// <summary>
+        /// Конец диапазона даты и времени действий.
+        /// </summary>
+        public DateTime EndTime { get; set; } = DateTime.MinValue;
+
+        /// <summary>
+        /// Действие над объектом.
+        /// </summary>
+        public ELogAction Action { get; set; } = ELogAction.LA_None;
+
+        /// <summary>
+        /// Идентификатор пользователя.
+        /// </summary>
+        public Guid UserId { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Идентификатор сущности.
+        /// </summary>
+        public Guid EntityType { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Первичный ключ.
+        /// </summary>
+        public Guid EntityPK { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Номер страницы.
+        /// </summary>
+        public int Page { get; set; } = 0;
+
+        /// <summary>
+        /// Количество элементов на странице.
+        /// </summary>
+        public int ItemsPerPage { get; set; } = 0;
+    }
+}
---
+++ Logger/LoggerService/Models/Log/LogModel.cs
@@ -0,0 +1,39 @@
+﻿namespace LoggerService.Models.Log
+{
+    public class LogModel
+    {
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        public long Id { get; set; } = 0;
+
+        /// <summary>
+        /// Идентификатор пользователя.
+        /// </summary>
+        public Guid UserId { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Дата и время действия.
+        /// </summary>
+        public DateTime Time { get; set; } = DateTime.MinValue;
+
+        /// <summary>
+        /// Действие над объектом.
+        /// </summary>
+        public Logger.DataAccess.Entities.ELogAction Action { get; set; } = Logger.DataAccess.Entities.ELogAction.LA_None;
+        /// <summary>
+        /// Идентификатор сущности.
+        /// </summary>
+        public Guid EntityType { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Первичный ключ.
+        /// </summary>
+        public Guid EntityPK { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Сущность.
+        /// </summary>
+        public string Entity { get; set; } = string.Empty;
+    }
+}
